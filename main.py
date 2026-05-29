21788599c2d191ed64c580a96f050c0d9b17a696
Praveen Mosesraj Solomon <praveenmosesrajsolomon@gmail.com>
2026-05-29 15:16:07 +0530

    Added advanced analytics and repository ranking system

--- main.py
+++ main.py
@@ -48,88 +48,133 @@ def fetch_github_repositories(username):
         return response.json()
     return []
 
+def calculate_total_stars(repositories):
+    total_stars = 0
+    for repo in repositories:
+        total_stars += repo.get('stargazers_count', 0)
 
-# User input and main logic
-username = input("Enter GitHub username: ")
-data = fetch_github_profile(username)
+    return total_stars
 
-# Display developer profile information
-if data:
+def get_most_starred_repositories(repositories):
+    if not repositories:
+        return None
+    return max(
+        repositories,
+        key=lambda repo: repo.get('stargazers_count', 0)
+    )
 
-    print("\nDeveloper Profile Information:")
+def get_top_repositories(repositories, top_n=5):
+    sorted_repos = sorted(
+        repositories,
+        key=lambda repo: repo.get('stargazers_count', 0),
+        reverse=True
+    )
+    return sorted_repos[:top_n]
 
-    print(f"Name: {data.get('name')}")
-    print(f"id: {data.get('id')}")
-    print(f"Username: {data.get('login')}")
-    print(f"Bio: {data.get('bio')}")
-    print(f"Location: {data.get('location')}")
-    print(f"Account Created At: {data.get('created_at')}")
-    print(f"Last Updated At: {data.get('updated_at')}")
-    print(f"Followers: {data.get('followers')}")
-    print(f"Following: {data.get('following')}")
-    print(f"Public Repositories: {data.get('public_repos')}")
-    print(f"GitHub URL: {data.get('html_url')}")
-else:
-    print("Failed to retrieve developer profile information.")
+def calculate_languages_used(repositories):
+    
+    languages = {}
 
-# Fetch and display repository analytics
+    """ Note: Syntax	Missing Key
+    repo.get("language")	Returns None
+    repo["language"]	Raises KeyError
 
-# Stars analysis, total stars, and repository details
-repos = fetch_github_repositories(username)
+    When to use which?
+    Use .get() when the key may not exist.
+    Use [] when the key must exist and missing data should be treated as an error.
+    In this case, since we are analyzing languages and some repositories might not
+    have a language specified, using .get() is safer to avoid KeyError and handle
+    missing data gracefully.
+    """
 
-print("\nDeveloper Repository Analytics:")
-print(f"\nTotal Repositories: {len(repos)}")
+    for repo in repositories:
+        # language = repo["language"]  # This would raise KeyError if "language" key is missing
+        language = repo.get("language")
 
-print(f"\nStars Analysis:\n")
+        if language:
 
-total_stars = 0
-for repo in repos:
-    print(f"Repository: {repo.get('name')}")
-    print(f"Stars: {repo.get('stargazers_count')}\n")
-    total_stars += repo.get('stargazers_count', 0)
+            if language in languages:
+                languages[language] += 1
+            else:
+                languages[language] = 1
 
-print(f"\nTotal Stars Across All Repositories: {total_stars}")
+    return languages
 
-# Top repositories
+def most_used_language(languages):
+    if not languages:
+        return None
+    return max(languages, key=languages.get)
 
-print("\nTop Repositories:")
+# --------------------------------------------------------------
+    # main function to run the application
+def main():
+    # User input and main logic
+    username = input("Enter GitHub username: ")
+    data = fetch_github_profile(username) # fetch developer profile information function call
 
-for repo in repos[:5]:  # Display top 5 repositories
-    print(f"\nRepository: {repo.get('name')}")
-    print(f"Stars: {repo.get('stargazers_count')}")
-    print(f"Language: {repo.get('language')}")
+    # Display developer profile information
+    if data:
 
-# Language Analytics:
+        print("\nDeveloper Profile Information:")
 
-print("\nLanguage Analytics:")
+        print(f"Name: {data.get('name')}")
+        print(f"id: {data.get('id')}")
+        print(f"Username: {data.get('login')}")
+        print(f"Bio: {data.get('bio')}")
+        print(f"Location: {data.get('location')}")
+        print(f"Account Created At: {data.get('created_at')}")
+        print(f"Last Updated At: {data.get('updated_at')}")
+        print(f"Followers: {data.get('followers')}")
+        print(f"Following: {data.get('following')}")
+        print(f"Public Repositories: {data.get('public_repos')}")
+        print(f"GitHub URL: {data.get('html_url')}")
+    else:
+        print("Failed to retrieve developer profile information.")
+    
+    # Fetch and display repository analytics:
 
-languages = {}
+    # Stars analysis, total stars, and repository details
 
-""" Note: Syntax	Missing Key
-repo.get("language")	Returns None
-repo["language"]	Raises KeyError
+    repos = fetch_github_repositories(username) # fetch github repositories function call
+    # fetched repositories stored in repos variable for further analysis
 
-When to use which?
-Use .get() when the key may not exist.
-Use [] when the key must exist and missing data should be treated as an error.
-In this case, since we are analyzing languages and some repositories might not
-have a language specified, using .get() is safer to avoid KeyError and handle
-missing data gracefully.
-"""
+    print("\nDeveloper Repository Analytics:")
 
-for repo in repos:
-    # language = repo["language"]  # This would raise KeyError if "language" key is missing
-    language = repo.get("language")
+    print(f"\nTotal Repositories: {len(repos)}")
 
-    if language:
+    print(f"\nStars Analysis:")
+    total_stars = calculate_total_stars(repos) # calculate total stars function call
+    print(f"\nTotal Stars Across All Repositories: {total_stars}")
 
-        if language in languages:
-            languages[language] += 1
-        else:
-            languages[language] = 1
+    # Most starred repositories
+    print("\nMost Starred Repositories:")
+    most_starred_repo = get_most_starred_repositories(repos) # get most starred repositories function call
+    if most_starred_repo:
+        print(f"\nMost Starred Repository: {most_starred_repo.get('name')}")
+        print(f"Stars: {most_starred_repo.get('stargazers_count')}")
+        print(f"Language: {most_starred_repo.get('language')}")
 
-print("\nLanguages Used:")
+    # Language Analytics:
+    print("\nLanguage Analytics:")
+    print("\nLanguages Used:")
+    languages = calculate_languages_used(repos) # calculate languages function call
+    for language, count in languages.items():
+        a = "repository" if count == 1 else "repositories"
+        print(f"{language}: {count} {a}")
 
-for language, count in languages.items():
-    a = "repository" if count == 1 else "repositories"
-    print(f"{language}: {count} {a}")
+    most_used_lang = most_used_language(languages) # most used language function call
+    if most_used_lang:
+        a = "repository" if languages[most_used_lang] == 1 else "repositories"
+        print(f"\nMost Used Language: {most_used_lang} ({languages[most_used_lang]} {a})")
+
+    print("\nTop 5 Repositories by Stars:") 
+    top_repos = get_top_repositories(repos) # get top repositories function call
+    for repo in top_repos:
+        print(f"\nRepository: {repo.get('name')} || Stars: {repo.get('stargazers_count')}")
+        print(f"Language: {repo.get('language')}")
+# --------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
+
+# --------------------------------------------------------------
